86171ec2a5dde35b79be9aa304d241116a3196db
jmo.py <33186474+JafetMo5601@users.noreply.github.com>
2021-03-28 15:53:04 -0600

    Functionality changes in the forms
    The form of the client menu, were fixed at the center of the screen and have different behaviors with the exit button.

--- AirlineApp/Code/src/airlineapp/Menus/ClientMenu.java
+++ AirlineApp/Code/src/airlineapp/Menus/ClientMenu.java
@@ -1,11 +1,5 @@
 package airlineapp.Menus;
 
-import airlineapp.StartWindowGUI;
-import airlineapp.iWindows;
-
-public class ClientMenu implements iWindows{
-    public void goBack(){
-        new StartWindowGUI().setVisible(true);
-    }
+public class ClientMenu {
     
 }
