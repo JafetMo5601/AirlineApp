e9a1440ce024a8a17c52d059ed68f78896c7e517
Mora Ugalde, Jafet <jafet.mora.ugalde@intel.com>
2021-03-28 16:49:50 -0600

    Testing changes for the push in new system.

--- Code/src/airlineapp/DBHandling/Encryptation.java
+++ Code/src/airlineapp/DBHandling/Encryptation.java
@@ -1,35 +1,80 @@
 package airlineapp.DBHandling;
 
+import airlineapp.Authentication.iAuthentication;
+import static airlineapp.DBHandling.DBManagement.userRetrieve;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.BadPaddingException;
 
-public class Encryptation {
-    public String getSHA256SecurePassword(String password, byte[] salt)
-    {
-        String generatedPassword = null;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public class Encryptation implements iAuthentication{
+
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+
+    public void setKey(String myKey) {
+        MessageDigest sha = null;
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(salt);
-            byte[] bytes = md.digest(password.getBytes());
-            StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++)
-            {
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-            }
-            generatedPassword = sb.toString();
-        } 
-        catch (NoSuchAlgorithmException e) 
-        {
-            e.printStackTrace();
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-256");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
         }
-        return generatedPassword;
+    }
+
+    public String encrypt(String strToEncrypt, String secret) {
+        try {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (UnsupportedEncodingException | InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException e) {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public String decrypt(String strToDecrypt, String secret) {
+        try {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (InvalidKeyException | NoSuchAlgorithmException | BadPaddingException | IllegalBlockSizeException | NoSuchPaddingException e) {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
     }
     
-    public byte [] getSalt(String token) throws NoSuchAlgorithmException {
-        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        byte[] salt = token.getBytes();
-        sr.nextBytes(salt);
-        return salt;
-    }    
+    public String generateEncryptedPassword(String email, String password){
+        String secretKey = generateSecretKey(email);
+        String encryptedPassword = encrypt(password, secretKey);
+        return encryptedPassword;
+    }
+    
+    @Override
+    public String generateSecretKey(String userEmail) {
+        try {
+            CallableStatement s = userRetrieve(userEmail);
+            return "" + s.getString("out_user_id").charAt(0)
+                    + s.getString("out_name").charAt(0)
+                    + s.getString("out_last_name").charAt(0);
+        } catch (SQLException ex) {
+            System.err.println("There was an issue running the stored procedure."
+                    + "\n Issue description: "+ ex.getMessage());
+        }
+        return null;
+    }
 }
---
+++ Code/src/airlineapp/Login/LoginSession.java
@@ -0,0 +1,7 @@
+package airlineapp.Login;
+
+public class LoginSession {
+    public static String name;
+    public static String email;
+    public static boolean isLoggedIn = false;    
+}
